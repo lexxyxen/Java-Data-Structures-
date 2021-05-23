d458a8cd208b504540b21dbd6a7aab14ab3b9beb
Allyx May Lichkov <allyxmaylichkov@Allyxs-MacBook-Pro.local>
2021-05-23 03:30:02 -0400

    Completed the node class implementation

--- LinkedList/src/Node.java
+++ LinkedList/src/Node.java
@@ -1,2 +1,32 @@
 public class Node<T extends Comparable<T>> {
+    private T data;
+    private Node<T> nextNode;
+
+    //Constructor
+    public Node(T data) {
+        this.data = data;
+    }
+
+    //Getter and Setters for the Node
+    public T getData() {
+        return data;
+    }
+
+    public Node<T> getNext() {
+        return nextNode;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public void setNext(Node<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    @Override
+    public String toString() {
+        return this.data.toString();
+    }
+
 }
